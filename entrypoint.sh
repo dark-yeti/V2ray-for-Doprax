07c3ebe9904f8e778c13f38f9f987bbf1ab322db
dark-yeti <116493228+dark-yeti@users.noreply.github.com>
2023-02-11 15:46:55 +0330

    echo running config

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 # Web Site:https://www.hicairo.com
 # Telegram:https://t.me/HiaiFeng
 
+echo "Set UUID to $UUID"
+echo "Set VMESS_WSPATH to $VMESS_WSPATH"
+echo "Set VLESS_WSPATH to $VLESS_WSPATH"
 sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g" /etc/v2ray/config.json
 sed -i "s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g" /etc/nginx/nginx.conf
 # exec "$@"
